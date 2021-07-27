8c6b8b602f728e959d60e1a4e6f8c87e0f336cdf
AlenGeoAlex <71920951+AlenGeoAlex@users.noreply.github.com>
2021-07-28 01:15:44 +0530

    Updated Validation For Database

--- src/main/java/me/alen_alex/bridgepractice/utility/Validation.java
+++ src/main/java/me/alen_alex/bridgepractice/utility/Validation.java
@@ -5,7 +5,7 @@ import me.alen_alex.bridgepractice.configurations.Configuration;
 public class Validation {
 
     public static boolean validateDatabase(){
-        if(Configuration.getHost() != null && Configuration.getUsername() != null && Configuration.getPort() != null && Configuration.getDatabase() != null)
+        if(Configuration.getHost().length() != 0 && Configuration.getUsername().length() != 0 && Configuration.getPort().length() != 0 && Configuration.getDatabase().length() != 0)
             return true;
         else
             return false;
